bf54ce65e3aed3649dcf35d3c99d4f18236f39d8
Adam Joyce <adamjoyce@outlook.com>
2017-08-24 20:24:54 +0100

    WIP - Making the spawning work for different resolutions and background sizes

--- chromatic/Assets/Scripts/BlockManager.cs
+++ chromatic/Assets/Scripts/BlockManager.cs
@@ -5,37 +5,42 @@ using UnityEngine;
 public class BlockManager : MonoBehaviour
 {
     public GameObject blockPrefab;          // The block that will make up each line.
-    public GameObject background;           // The background used to determine spacing between blocks in the line.
-    public GameObject spawnPosition;        // The starting spawn location for the line of blocks.
+    public Renderer backgroundRenderer;     // The background's renderer component - used for block spacing and start position.
     public int numberOfBlocks = 5;          // The number of blocks each line is made up of.
 
     private GameObject[] blocks;            // The array of blocks.
+    private Vector3 spawnPosition;          // The starting spawn location for the line of blocks.
 
     /* Use this for initialization. */
     void Start()
     {
         blocks = new GameObject[numberOfBlocks];
+
+        // X screen position for the bottom left of the background element.
+        float backgroundExtentX = Camera.main.WorldToScreenPoint(new Vector3(backgroundRenderer.bounds.min.x, 0, 0)).x;
+        spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(backgroundExtentX, -1, 10));
+
+        // Account for the block width.
+        //float blockWidth = blockPrefab.GetComponent<Renderer>().bounds.size.x;
+        //spawnPosition.x += blockWidth * 0.5f;
+
         SpawnBlocks();
-    }
-
-    /* Update is called once per frame. */
-    void Update()
-    {
-
     }
 
     /* Spawns a line of blocks. */
     private void SpawnBlocks()
     {
-        float blockSizeX = blockPrefab.GetComponent<Renderer>().bounds.size.x;
-        float gapSize = ((background.GetComponent<Renderer>().bounds.size.x - (blockSizeX * numberOfBlocks)) / numberOfBlocks);
+        float blockWidth = blockPrefab.GetComponent<Renderer>().bounds.size.x;
+        float gapSize = ((backgroundRenderer.bounds.size.x - (blockWidth * numberOfBlocks)) / (numberOfBlocks + 1));
+        spawnPosition.x += gapSize;
         for (int i = 0; i < numberOfBlocks; ++i)
         {
             // Adjust the spawn position based on which block is being spawned in the line.
-            Vector3 position = spawnPosition.transform.position;
-            position.x += (i * blockSizeX) + (i * gapSize);
+            Vector3 position = spawnPosition;
+            position.x += (i * blockWidth) + (i * gapSize);
 
             Instantiate(blockPrefab, position, Quaternion.identity);
         }
+        spawnPosition.x -= gapSize;
     }
 }
