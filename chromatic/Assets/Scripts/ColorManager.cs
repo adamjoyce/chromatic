1d5d785bb37af11597407e2d2b5805c0cc842651
Adam Joyce <adamjoyce@outlook.com>
2017-08-27 11:56:53 +0100

    WIP - Block colors

---
+++ chromatic/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour 
+{
+    public Color[] colors;          // The list of colors the blocks or background can take.
+
+	/* Use this for initialization. */
+	private void Start() 
+	{
+
+	}
+	
+	/* Update is called once per frame. */
+	private void Update() 
+	{
+		
+	}
+}
