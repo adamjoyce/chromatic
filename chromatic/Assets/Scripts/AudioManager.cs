9f33e8e57f60978e2f935f59167c9dd83af1af12
Adam Joyce <adamjoyce@outlook.com>
2017-09-05 16:34:10 +0100

    WIP - Audio

---
+++ chromatic/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour 
+{
+    public Sound[] sounds;              // The array of all non-position dependent sounds in the game.
+
+	/* Use this for initialization. */
+	private void Start() 
+	{
+		
+	}
+	
+	/* Update is called once per frame. */
+	private void Update() 
+	{
+		
+	}
+}
